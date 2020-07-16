026c2e73f006770d3014a27aa934c1e9678d8065
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-07-15 20:46:38 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        tuna member1 = new tuna("megan", "fox");
+        tuna member2 = new tuna("Nat","Port");
+        tuna member3 = new tuna("tay","swift");
+
+        System.out.println();
+        System.out.println(member1.getFirst());
+        System.out.println(member1.getLast());
+        System.out.println(member1.getMembers());
+
+
+
+
+    }
+}
